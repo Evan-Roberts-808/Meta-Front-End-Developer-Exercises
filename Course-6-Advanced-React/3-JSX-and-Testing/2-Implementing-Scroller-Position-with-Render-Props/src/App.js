e28a74b33c684f31d1f3a396fbc4df1d6fc212cd
Evan <eroberts1998@icloud.com>
2023-02-21 16:03:20 -0500

    adding second exercise from course 6 module 3

---
+++ Course-6-Advanced-React/3-JSX-and-Testing/2-Implementing-Scroller-Position-with-Render-Props/src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const MousePosition = ({ render }) => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    const handleMousePositionChange = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", handleMousePositionChange);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMousePositionChange);
+    };
+  }, []);
+
+  return render({mousePosition})
+};
+
+const PanelMouseLogger = ({}) => {
+  return (
+    <div className="BasicTracker">
+      <p>Mouse position:</p>
+      <div className="Row">
+        <span>x: {mousePosition.x}</span>
+        <span>y: {mousePosition.y}</span>
+      </div>
+    </div>
+  );
+};
+
+const PointMouseLogger = ({mousePosition}) => {
+  return (
+    <p>
+      ({mousePosition.x}, {mousePosition.y})
+    </p>
+  )
+};
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Header">Little Lemon Restaurant 🍕</header>
+      <PanelMouseLogger />
+      <PointMouseLogger />
+    </div>
+  );
+}
+
+export default App;

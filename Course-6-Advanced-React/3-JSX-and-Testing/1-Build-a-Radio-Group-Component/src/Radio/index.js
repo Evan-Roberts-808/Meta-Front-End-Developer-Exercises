70a81e2628ce4558e4d3eae8dbd44231db46de4e
Evan <eroberts1998@icloud.com>
2023-02-21 10:02:58 -0500

    adding first exercise from course 6 module 3

---
+++ Course-6-Advanced-React/3-JSX-and-Testing/1-Build-a-Radio-Group-Component/src/Radio/index.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import "./styles.css";
+
+export const RadioGroup = ({ onChange, selected, children }) => {
+  const RadioOptions = React.Children.map(children, (child) => {
+    return React.cloneElement(child, {
+      onChange,
+      checked: child.props.value === selected,
+    });
+  });
+
+  return <div className="RadioGroup">{RadioOptions}</div>;
+};
+
+export const RadioOption = ({ value, checked, onChange, children }) => {
+  return (
+    <div className="RadioOption">
+      <input
+        id={value}
+        type="radio"
+        name={value}
+        value={value}
+        checked={checked}
+        onChange={(e) => {
+          onChange(e.target.value);
+        }}
+      />
+      <label htmlFor={value}>{children}</label>
+    </div>
+  );
+};
